0c2cc253ba82098ceadaad471523abff12485747
Elliee <ellieprog29@gmail.com>
2025-01-25 17:46:01 -0300

    Level Scene Setup

--- Assets/_Game/_Scripts/Oxygen.cs
+++ Assets/_Game/_Scripts/Oxygen.cs
@@ -11,22 +11,22 @@ public class Oxygen : MonoBehaviour
 
     void Start()
     {
-        text.text = totalOxygen + "%";
-        InvokeRepeating("LoseOxygen", interval, interval);
+        if (text != null) text.text = totalOxygen + "%";
+        InvokeRepeating(nameof(LoseOxygen), interval, interval);
     }
 
     void LoseOxygen()
     {
         if (totalOxygen > 0) totalOxygen -= loss;
         else totalOxygen = 0;
-        text.text = totalOxygen + "%";
+        if (text != null) text.text = totalOxygen + "%";
     }
 
     public void AddOxygen(float amount)
     {
         totalOxygen += amount;
         if (totalOxygen > 100) totalOxygen = 100;
-        text.text = totalOxygen + "%";
+        if (text != null) text.text = totalOxygen + "%";
     }
     public float ReturnAmount()
     {
